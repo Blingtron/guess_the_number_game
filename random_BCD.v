51616f8447ca287899ec8b77e6ad22aae6bb6f07
Shane <skirkley268@gmail.com>
2016-12-04 15:52:48 -0700

    -_-

--- random_BCD.v
+++ random_BCD.v
@@ -8,11 +8,11 @@ module random_BCD(Q0, Q1, Qneg, rnd0, rnd1, rndneg, state, Clock);
 	always @(posedge Clock) begin
 	if (state == 0) begin
 		
-		if (Q0[8:5] < 4'd9) begin 
+		if (Q0[8:5] <= 4'd9) begin 
 			rnd0 <= Q0[8:5];
 			rndneg <= Q0[0];
 		end
-		if (Q1[8:5] < 4'd9) begin
+		if (Q1[8:5] <= 4'd9) begin
 			rnd1 <= Q1[8:5];
 			rndneg <= Q1[0];
 		end
